81a42ca4c5e083f1af322c4d16a5b7b66aa5f4e3
clonez9494 <clonez9494@gmail.com>
2022-09-05 13:55:27 +0700

    drawer add

--- lib/modules/home/views/home_page.dart
+++ lib/modules/home/views/home_page.dart
@@ -8,7 +8,7 @@ import '../controllers/home_controller.dart';
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
 
-  _header(data) {
+  Widget _header(data) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 6.w),
       child: Column(
@@ -60,56 +60,80 @@ class HomePage extends GetView<HomeController> {
               child: GridView(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   childAspectRatio: 4,
-                  crossAxisSpacing: 4,
+                  crossAxisSpacing: 6,
                   mainAxisSpacing: 4,
                   crossAxisCount: 2,
                 ),
                 children: [
                   Card(
                       color: kBlueColor,
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
                       child: Row(
                         children: [
-                          SizedBox(width: 2.w),
+                          SizedBox(width: 3.w),
                           Icon(Icons.directions_boat_rounded,
                               color: Colors.white),
                           SizedBox(width: 2.w),
                           TextApp("Port Services",
-                              color: Colors.white, maxLines: 2),
+                              fontWeight: bold,
+                              color: Colors.white,
+                              maxLines: 2),
+                          SizedBox(width: 3.w),
                         ],
                       )),
                   Card(
                       color: kBlueColor,
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
                       child: Row(
                         children: [
-                          SizedBox(width: 2.w),
+                          SizedBox(width: 3.w),
                           Icon(Icons.local_shipping_rounded,
                               color: Colors.white),
                           SizedBox(width: 2.w),
                           TextApp("Transpotation Services",
-                              color: Colors.white, maxLines: 2),
+                              fontWeight: bold,
+                              color: Colors.white,
+                              maxLines: 2),
+                          SizedBox(width: 3.w),
                         ],
                       )),
                   Card(
-                      color: kBlueColor,
+                      color: Colors.grey,
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
                       child: Row(
                         children: [
-                          SizedBox(width: 2.w),
+                          SizedBox(width: 3.w),
                           Icon(Icons.home_filled, color: Colors.white),
                           SizedBox(width: 2.w),
                           TextApp("Warehouse Services",
-                              color: Colors.white, maxLines: 2),
+                              color: Colors.white,
+                              maxLines: 2,
+                              fontWeight: bold),
+                          SizedBox(width: 3.w),
                         ],
                       )),
                   Card(
-                      color: kBlueColor,
+                      color: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      elevation: 6,
                       child: Row(
                         children: [
-                          SizedBox(width: 2.w),
+                          SizedBox(width: 3.w),
                           Icon(Icons.local_airport_rounded,
                               color: Colors.white),
                           SizedBox(width: 2.w),
                           TextApp("Air Cargo Services",
-                              color: Colors.white, maxLines: 2),
+                              color: Colors.white,
+                              maxLines: 2,
+                              fontWeight: bold),
+                          SizedBox(width: 3.w),
                         ],
                       )),
                 ],
@@ -119,65 +143,71 @@ class HomePage extends GetView<HomeController> {
     );
   }
 
-  _body() {
+  Widget _body() {
     return Column();
   }
 
   Widget _drawer() {
-    return SizedBox(
-      //membuat menu drawer
-      child: Drawer(
-        //membuat list,
-        //list digunakan untuk melakukan scrolling jika datanya terlalu panjang
-        child: ListView(
-          padding: EdgeInsets.zero,
-          //di dalam listview ini terdapat beberapa widget drawable
-          children: [
-            UserAccountsDrawerHeader(
-              //membuat gambar profil
-              currentAccountPicture: Image(
-                  image: NetworkImage(
-                      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")),
-              //membuat nama akun
-              accountName: Text("Sahretech"),
-              //membuat nama email
-              accountEmail: Text("ig: @sahretech"),
-              //memberikan background
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://cdn.pixabay.com/photo/2016/04/24/20/52/laundry-1350593_960_720.jpg"),
-                      fit: BoxFit.cover)),
-            ),
-            //membuat list menu
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Beranda"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.people),
-              title: Text("Pegawai"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.money),
-              title: Text("Transaksi"),
-              onTap: () {},
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.emoji_emotions),
-              title: Text("Profil"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.info),
-              title: Text("Tentang"),
-              onTap: () {},
-            ),
-          ],
-        ),
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: Image(
+                image: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")),
+            accountName: Text("Sahretech"),
+            accountEmail: Text("ig: @sahretech"),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2016/04/24/20/52/laundry-1350593_960_720.jpg"),
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: Icon(Icons.manage_accounts_sharp),
+            title: Text("My Account"),
+            onTap: () {
+              Get.back();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.switch_account_sharp),
+            title: Text("Booking"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.screenshot_monitor_outlined),
+            title: Text("Operational Monitoring"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.query_stats_outlined),
+            title: Text("Tarif Info"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.currency_exchange),
+            title: Text("Invoice"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.file_present_rounded),
+            title: Text("List Hold Amount"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.document_scanner),
+            title: Text("List Proforma (DP)"),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Logout"),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
