b843327158338a7962213d581fdac43ab646b887
clonez9494 <clonez9494@gmail.com>
2022-09-15 21:06:19 +0700

    FIX: resolve snackbar spam

--- lib/helpers/system/snackbar.dart
+++ lib/helpers/system/snackbar.dart
@@ -10,65 +10,73 @@ class AppSnackBar {
       required Icon icon,
       SnackPosition snackPosition = SnackPosition.BOTTOM,
       int seconds = 3}) {
-    Get.snackbar(
-      title,
-      message,
-      icon: icon,
-      colorText: colorText,
-      backgroundColor: backgroundColor,
-      forwardAnimationCurve: Curves.fastOutSlowIn,
-      snackPosition: snackPosition,
-      shouldIconPulse: true,
-      margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
-      isDismissible: true,
-      duration: Duration(seconds: seconds),
-    );
+    if (!Get.isSnackbarOpen) {
+      Get.snackbar(
+        title,
+        message,
+        icon: icon,
+        colorText: colorText,
+        backgroundColor: backgroundColor,
+        forwardAnimationCurve: Curves.fastOutSlowIn,
+        snackPosition: snackPosition,
+        shouldIconPulse: true,
+        margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
+        isDismissible: true,
+        duration: Duration(seconds: seconds),
+      );
+    }
   }
 
   static void success(String message, {seconds}) {
-    Get.snackbar(
-      "Success",
-      message,
-      icon: Icon(Icons.check_circle_outline_rounded, color: Colors.white),
-      colorText: Colors.white,
-      backgroundColor: Colors.green[900],
-      forwardAnimationCurve: Curves.fastOutSlowIn,
-      snackPosition: SnackPosition.TOP,
-      shouldIconPulse: true,
-      margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
-      isDismissible: true,
-      duration: Duration(seconds: seconds ?? 3),
-    );
+    if (!Get.isSnackbarOpen) {
+      Get.snackbar(
+        "Success",
+        message,
+        icon: Icon(Icons.check_circle_outline_rounded, color: Colors.white),
+        colorText: Colors.white,
+        backgroundColor: Colors.green[900],
+        forwardAnimationCurve: Curves.fastOutSlowIn,
+        snackPosition: SnackPosition.TOP,
+        shouldIconPulse: true,
+        margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
+        isDismissible: true,
+        duration: Duration(seconds: seconds ?? 3),
+      );
+    }
   }
 
   static void info(String message) {
-    Get.snackbar(
-      "Info",
-      message,
-      colorText: Colors.white,
-      backgroundColor: Colors.black,
-      forwardAnimationCurve: Curves.fastLinearToSlowEaseIn,
-      reverseAnimationCurve: Curves.linear,
-      snackPosition: SnackPosition.BOTTOM,
-      shouldIconPulse: true,
-      isDismissible: true,
-      duration: Duration(seconds: 3),
-    );
+    if (!Get.isSnackbarOpen) {
+      Get.snackbar(
+        "Info",
+        message,
+        colorText: Colors.white,
+        backgroundColor: Colors.black,
+        forwardAnimationCurve: Curves.fastLinearToSlowEaseIn,
+        reverseAnimationCurve: Curves.linear,
+        snackPosition: SnackPosition.BOTTOM,
+        shouldIconPulse: true,
+        isDismissible: true,
+        duration: Duration(seconds: 3),
+      );
+    }
   }
 
   static void error(String message, {String? title}) {
-    Get.snackbar(
-      title ?? "Error",
-      message,
-      icon: Icon(Icons.error, color: Colors.white),
-      colorText: Colors.white,
-      backgroundColor: Colors.red[900],
-      forwardAnimationCurve: Curves.fastOutSlowIn,
-      snackPosition: SnackPosition.TOP,
-      shouldIconPulse: true,
-      margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
-      isDismissible: true,
-      duration: Duration(seconds: 3),
-    );
+    if (!Get.isSnackbarOpen) {
+      Get.snackbar(
+        title ?? "Error",
+        message,
+        icon: Icon(Icons.error, color: Colors.white),
+        colorText: Colors.white,
+        backgroundColor: Colors.red[900],
+        forwardAnimationCurve: Curves.fastOutSlowIn,
+        snackPosition: SnackPosition.TOP,
+        shouldIconPulse: true,
+        margin: EdgeInsets.only(bottom: 5, left: 20, right: 20),
+        isDismissible: true,
+        duration: Duration(seconds: 3),
+      );
+    }
   }
 }
